a505a7266db62ed0bbbebc0405704b6d7f95b49c
ivanramadhlan <62657101+ivanramadhlan@users.noreply.github.com>
2020-03-25 21:37:36 +0700

    Add files via upload

---
+++ materi_3_percobaan_1_M.IVAN RAMADHLAN.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int bil;
+	cout<<"masukkan sebuah bilangan:";
+	cin>>bil;
+	
+	if(bil >= 0)
+		cout<<bil<<"adalah bilangan POSISTIF"<<endl;
+		else
+		cout<<bil<<"adalah bilangan NEGATIF"<<endl;
+		
+		system("pause");
+		return 0;
+}
